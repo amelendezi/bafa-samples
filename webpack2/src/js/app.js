9293d1fecb138ba724f3b7976a705b2ecb12eed0
Alberto Melendez <amelendez.es@gmail.com>
2017-07-10 22:17:35 +0200

    Webpack sample

---
+++ webpack2/src/js/app.js
@@ -0,0 +1,26 @@
+var showSecret = false;
+
+secretButton.addEventListener('click', toggleSecretState);
+updateSecretParagraph();
+
+function toggleSecretState() {
+    showSecret = !showSecret;
+    updateSecretParagraph();
+    updateSecretButton();
+}
+
+function updateSecretButton() {
+    if (showSecret) {
+        secretButton.textContent = 'Hide the secret';
+    } else {
+        secretButton.textContent = 'Show the secret';
+    }
+}
+
+function updateSecretParagraph() {
+    if(showSecret) {
+        secretParagraph.style.display = 'block';
+    } else {
+        secretParagraph.style.display = 'none';
+    }
+}
